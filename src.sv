c71d5fa168417a9625e1666212e5cda0d978f79b
Emil Yuxue Tan <c3271566@uon.edu.au>
2020-11-17 16:54:15 +1100

    Edited excel file

--- src.sv
+++ src.sv
@@ -1,16 +1,58 @@
-/********************TOP MODULE********************/
-module src (
-	input  logic [9:0] SW,
-	input  logic [3:0] KEY,
-	output logic [7:0] LEDG, 
-	output logic [9:0] LEDR,
-	output logic [6:0] HEX0, HEX1, HEX2, HEX3	
-);
-
+// TOP LEVEL MODULE
+module src (); 
 endmodule
-/**************************************************/
-/********************R-TYPE ALU********************/
-module RALU #(parameter n = 4) (
+// CPU
+module CPU (
+	input  logic  clk, 
+);		
+
+	ROM #() rom(, instruction); 
+	assign rd = instruction[3:0]; 
+	assign rt = instruction[7:4]; 
+	assign rs = instruction[11:8]; 
+	assign op = instruction[17:12];
+
+endmodule 
+// ROM
+module ROM #(parameter m = 7, w = 4) (
+	input  logic [m - 1:0] Ad; 
+	output logic [w - 1:0] Dout
+); 
+
+	logic [w - 1:0] mem[2**m - 1:0]; 
+	assign Dout = mem[Ad]; 
+	
+	initial begin
+		$readmemb("", mem); 
+	end 
+
+endmodule 
+// Program Counter
+module PC (
+	input  logic clk, 
+	output logic [31:0] q
+); 
+
+	always_ff @(posedge clk) 
+		q <= q + 1; 
+
+endmodule 
+// Opcode Priority 
+module OP (
+	input  logic [4:0] opcode, 
+	output logic rtype, branch1, jump, branch2, itype, mem
+); 
+
+	assign jump    = (~opcode[4] & ~opcode[3] & ~opcode[2] & ~opcode[1] & ~opcode[0]); 
+	assign branch1 = (~opcode[4] & ~opcode[3] & ~opcode[2] & ~opcode[1] &  opcode[0]); 
+	assign branch2 = (~opcode[4] & ~opcode[3] & ~opcode[2] &  opcode[1]); 
+	assign itype   = (~opcode[4] & ~opcode[3] &  opcode[2]); 
+	assign mem     = (~opcode[4] &  opcode[3]);
+	assign rtype   =   opcode[4]; 
+
+endmodule 
+// ALU
+module ALU #(parameter n = 4) (
 	input  logic [3:0] F, 
 	input  logic [n - 1:0] A, B,
 	output logic [n - 1:0] Y, 
@@ -30,7 +72,65 @@ module RALU #(parameter n = 4) (
 	mux2to1 #(n) mux1(F[2], AUout, logicOut, Y);
 	
 endmodule 
+// Multiply-Division 
+module multdiv #(parameter n = 4) (
+	input  logic [n - 1:0] a, b, 
+	input  logic clk,
+	input  logic [3:0] F, 
+	output logic [n - 1:0] Y, hiNext, loNext
+);
 
+	logic en1, en2; 
+	logic [n - 1:0] hi, lo, h, l, r, q; 
+
+	assign en1 = (); 
+	assign en2 = ();
+
+	assign {h, l} = a * b; 
+	assign  r     = a % b; 
+	assign  q     = a / b; 
+
+	mux2to1 #(n) mux0(F[1], hiNext, loNext, Y);
+	mux2to1 #(n) mux1(F[1], h, r, hi);
+	mux2to1 #(n) mux2(F[1], l, q, lo);
+
+	always_ff @(posedge clk) 
+		begin
+			if(en1) hiNext <= F[3]? hi : a;
+			if(en2) loNext <= F[3]? lo : a;
+		end
+
+endmodule
+// Shifter 
+module shifter #(parameter n = 4) (
+	input  logic [1:0] F, 
+	input  logic [2**n - 1:0] A, 
+	input  logic [n - 1:0] Sh, 
+	output logic [2**n - 1:0] Y 
+);
+
+	logic [2**n - 1:0] D0, D1; 
+	
+	assign D0 = F[0]?  (A >> Sh)           : (A << Sh);
+	assign D1 = F[0]?  ($signed(A) >>> Sh) : (A);
+	assign Y  = F[1]?  D1                  : D0;
+
+endmodule
+// Immediate operand extension 
+module IE ();
+endmodule; 
+// Register call function 
+module regfile (); 
+
+	logic [15:0] rf[15:0]; 
+	always_ff @(posedge clk) 
+		rf[RA1_WA] <= WD; 
+	assign RD1 = rf[RA1_WA]; 
+	assign RD2 = rf[RA2]; 
+	assign BTD = rf[BTA];
+
+endmodule 
+// Adder with overflow 
 module OVadder #(parameter n = 4) (
 	input  logic [1:0] s, 
 	input  logic [n - 1:0] a, b, 
@@ -49,7 +149,7 @@ module OVadder #(parameter n = 4) (
 	mux2to1 #(n) mux1(s[0], OVs, OVu, OV);
 
 endmodule
-
+// Set less than 
 module slt #(parameter n = 4) (
 	input  logic  OVs, Cout, s0, s2,
 	input  logic [n - 1:0] S,
@@ -67,60 +167,7 @@ module slt #(parameter n = 4) (
 	mux2to1 #(n) mux2 (s2, S, z, AUout); 
 
 endmodule 
-/**************************************************/
-/******************R-TYPE MULTDIV******************/
-module rmultdiv #(parameter n = 4) (
-	input  logic [n - 1:0] a, b, 
-	input  logic clk,
-	input  logic [3:0] F, 
-	output logic [n - 1:0] Y, hiNext, loNext
-);
-
-logic en1, en2; 
-logic [n - 1:0] hi, lo, h, l, r, q; 
-
-assign en1 = (
-	(~F[3] & ~F[2] & ~F[1] &  F[0]) |
-	( F[3] & ~F[2] & ~F[1] & ~F[0]) 
-); 
-assign en2 = (
-	(~F[3] & ~F[2] &  F[1] &  F[0]) |
-	( F[3] & ~F[2] &  F[1] & ~F[0])
-);
-
-assign {h, l} = a * b; 
-assign  r     = a % b; 
-assign  q     = a / b; 
-
-mux2to1 #(n) mux0(F[1], hiNext, loNext, Y);
-mux2to1 #(n) mux1(F[1], h, r, hi);
-mux2to1 #(n) mux2(F[1], l, q, lo);
-
-always_ff @(posedge clk) 
-	begin
-		if(en1) hiNext <= F[3]? hi : a;
-		if(en2) loNext <= F[3]? lo : a;
-	end
-
-endmodule
-/**************************************************/
-/*****************R-TYPE SHIFTER*******************/
-module shifter #(parameter n = 4) (
-	input  logic [1:0] F, 
-	input  logic [2**n - 1:0] A, 
-	input  logic [n - 1:0] Sh, 
-	output logic [2**n - 1:0] Y 
-);
-
-	logic [2**n - 1:0] D0, D1; 
-	
-	assign D0 = F[0]?  (A >> Sh)           : (A << Sh);
-	assign D1 = F[0]?  ($signed(A) >>> Sh) : (A);
-	assign Y  = F[1]?  D1                  : D0;
-
-endmodule
-/**************************************************/
-/*********************MISC*************************/
+// 4 to 1 mux 
 module mux4to1 #(parameter W = 2) (
 	input  logic [W - 1:0] d0, d1, d2, d3,
 	input  logic [1:0] s,
@@ -134,7 +181,7 @@ module mux4to1 #(parameter W = 2) (
 	assign y  = s[1]?  hi : lo;
 	
 endmodule
-
+// 2 to 1 mux 
 module mux2to1 #(parameter W = 1) (
 	input  logic s, 
 	input  logic [W - 1:0] d0, d1,
@@ -144,7 +191,7 @@ module mux2to1 #(parameter W = 1) (
 	assign y = s?  d1 : d0;
 
 endmodule
-
+// Adder 
 module addsub #(parameter W = 4) (
 	input  logic [W - 1:0] A, B,
 	input  logic s,
@@ -157,4 +204,3 @@ module addsub #(parameter W = 4) (
 	assign {cout,Y} = A + b + s;
 
 endmodule 
-/**************************************************/
